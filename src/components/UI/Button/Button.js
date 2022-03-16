ddb16b0999a76c93178a465c3c295685bb1aa71a
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-16 17:45:37 +0530

    css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,18 @@
 import React from "react";
-import styledComponents from "styled-components";
-import "./Button.css";
+
+//CSS MODULES
+import styles from "./Button.module.css";
+//import like this, and change css file name to name.module.css
+//css modules are better than styled components
+//it keeps the css code seperate
+//and we can choose which style or class to choose from which css file
+//here in import, styles is an object
+//and all classes as there as properties of that object
+//so it solves the problem and keeps the css code seperate
+
+
+//STYLED COMPONENTS
+//import styledComponents from "styled-components";
 //to install styled components package
 //npm install --save styled-components
 //used to create components with its own css code.
@@ -12,40 +24,42 @@ import "./Button.css";
 //button`` is like a function. similarly div, p and all present
 //syntax to create a styled component and css code goes between commas
 //adding media querries - styling based on device size
-const Button = styledComponents.button`
+// const Button = styledComponents.button`
 
-width: 100%;
-@media(min-width:768px){
-width:auto;
-}
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// width: 100%;
+// @media(min-width:768px){
+// width:auto;
+// }
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
 
-&:focus {
-  outline: none;
-}
+// &:focus {
+//   outline: none;
+// }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
 
-`;
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  //styles.className
+  //if another css file imported also, then styles1.className similarly
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
