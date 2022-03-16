af41f81bcac3fb0995f4072508f9d25f57213be6
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-16 16:00:16 +0530

    css dynamic classes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,27 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+  const [isValid,setIsValid]=useState(true);
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length>0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
-
+//how to use css dynamic classes
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* ``is used for string interpolation using $ sign */}
+      <div className={`form-control ${isValid?'':'invalid'}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
